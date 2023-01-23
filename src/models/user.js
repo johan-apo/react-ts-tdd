7b4ee5ff0d8cd770337cac3271ff865f284dd10b
Johan Apolinario <johelapo@gmail.com>
2023-01-22 21:40:16 -0500

    user model removed

--- src/models/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.DefaultPrivacyLevel = void 0;
-var DefaultPrivacyLevel;
-(function (DefaultPrivacyLevel) {
-    DefaultPrivacyLevel["public"] = "public";
-    DefaultPrivacyLevel["private"] = "private";
-    DefaultPrivacyLevel["contacts"] = "contacts";
-})(DefaultPrivacyLevel = exports.DefaultPrivacyLevel || (exports.DefaultPrivacyLevel = {}));
